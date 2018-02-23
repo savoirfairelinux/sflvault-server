2b868bc65d6840270b67aacf51746dd087b795b0
Ernesto Rodriguez Ortiz <ernesto.rodriguezortiz@savoirfairelinux.com>
2018-02-23 14:58:07 -0500

    Fix some QA errors

--- vaultsrv/admin.py
+++ vaultsrv/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-
-from vaultsrv.models import (
-    Account, AccountGroup, GroupMembership, Customer, ServiceGroup, Service,
-    ServiceGroupMembership
-)
+from vaultsrv.models import (Account, AccountGroup, Customer, GroupMembership,
+                             Service, ServiceGroup, ServiceGroupMembership)
 
 # This is no the final admin definition, it's is only to test the models
 admin.site.register((
--- vaultsrv/models/users.py
+++ vaultsrv/models/users.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-
 __all__ = ['Account', 'AccountGroup', 'GroupMembership']
 
 
